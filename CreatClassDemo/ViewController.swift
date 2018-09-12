87da62dee4b4cc3913e6816f6122792541b11f47
bestsu <bestsu@best-sudemacbook-pro.local>
2018-09-12 11:06:38 +0800

    Initial Commit

---
+++ CreatClassDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreatClassDemo
+//
+//  Created by bestsu on 2018/9/12.
+//  Copyright © 2018年 bestsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
